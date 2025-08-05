d33bb503cebd25b21207431d96618d137792e52b
Omid Hajati <omidhajati@gmail.com>
2025-08-04 18:29:24 -0700

    Add Model.py script

---
+++ Model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
+def train_arima_model(series: pd.Series, order=(2,1,2)):
+    """
+    Fit an ARIMA model to a time series.
+
+    Args:
+        series (pd.Series): Time series data.
+        order (tuple): ARIMA(p,d,q) order.
+
+    Returns:
+        model_fit: Fitted ARIMA model.
+    """
+    model = ARIMA(series, order=order)
+    model_fit = model.fit()
+    return model_fit
+
+def forecast_arima(model_fit, steps=30):
+    """
+    Forecast future values using a trained ARIMA model.
+
+    Args:
+        model_fit: Trained ARIMA model.
+        steps (int): Number of steps to forecast.
+
+    Returns:
+        pd.Series: Forecasted values.
+    """
+    forecast = model_fit.forecast(steps=steps)
+    return forecast
+
